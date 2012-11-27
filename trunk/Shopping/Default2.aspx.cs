253d0913b9cd549432e360b9230153272143c412
angvishvish <angvishvish@ee9c0cdc-4213-492d-91f3-4df673faede1>
2012-11-27 20:52:57 +0000

    just checking..

--- trunk/Shopping/Default2.aspx.cs
+++ trunk/Shopping/Default2.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 public partial class Default2 : System.Web.UI.Page
 {
-    protected void Page_Load(object sender, EventArgs e)
+    protected void (object sender, EventArgs e)
     {
 
     }
